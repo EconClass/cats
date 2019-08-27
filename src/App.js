c295c40977c33e82e3aeeac52e7a96f71f6419a4
EconClass <bizmail327@gmail.com>
2019-08-27 15:05:39 -0700

    header component built

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PageHeader from './PageHeader';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <PageHeader />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/PageHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageHeader.css';
+
+function PageHeader() {
+  return (
+    <div>
+      <h1>Cats Galore!</h1>
+    </div>
+  );
+};
+
+export default PageHeader;
