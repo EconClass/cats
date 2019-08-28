e9e6631d5051e1cfc48865817e8ba58206468516
EconClass <bizmail327@gmail.com>
2019-08-27 18:02:05 -0700

    [fin] base functionality

--- src/PageContent.js
+++ src/PageContent.js
@@ -4,7 +4,7 @@ import './PageContent.css';
 
 function PageContent() {
   return (
-    <div>
+    <div className="grid-container">
       <Project title="Tetris Dots" image="/images/kitten-0.jpeg" link="#" />
       <Project title="Zombie Server" image="/images/kitten-1.jpeg" link="#" />
       <Project title="Amazing Colors" image="/images/kitten-2.jpeg" link="#" />
